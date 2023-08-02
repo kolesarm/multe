7ee6622eb549e6ab196410b3700e5a99e843f1bc
Michal Kolesár <kolesarm@proton.me>
2023-08-02 16:02:38 -0400

    Add codecov

--- tests/testthat/test-EHW-check.R
+++ tests/testthat/test-EHW-check.R
@@ -9,9 +9,9 @@ test_that("Test Fryer and Levitt", {
         estidx <- 0:((NROW(x$est_f)/3-1))*3+1
         ## Convert to HC1 errors
         if (cluster)
-            HC1 <- sqrt((x$n_f-1)/(x$n_f-x$k_f-1))
+            HC1 <- sqrt((x$n_f-1)/ (x$n_f-x$k_f-1))
         else
-            HC1 <- sqrt(x$n_f/(x$n_f-x$k_f-1))
+            HC1 <- sqrt(x$n_f/ (x$n_f-x$k_f-1))
         testthat::expect_identical(unname(x$t_f[[2]]), chi[[2]])
         c(rd(x$est_f[estidx, 1], est), rd(HC1*x$est_f[estidx+1, 1], se),
           rd(x$t_f$W, chi[[1]]), abs(x$t_f[[3]]- chi[[3]]))
@@ -23,5 +23,6 @@ test_that("Test Fryer and Levitt", {
                      list(44.380661, 16L, 0.00017), cluster=TRUE)
     testthat::expect_identical(t1 <= c(1e-8, 4e-4, 2e-4, 5e-5),
                                rep(TRUE, 4))
-    ## TODO: Check ATE one at a time. Clustering, not weighted, no controls, no overlap
+    ## TODO: Check ATE one at a time. Clustering, not weighted, no controls, no
+    ## overlap
 })
